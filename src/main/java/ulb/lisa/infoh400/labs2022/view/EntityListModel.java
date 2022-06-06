6be2ad0933e7baa339dfd441096c1a1147038966
maximedg9 <91827637+maximedg9@users.noreply.github.com>
2022-06-06 10:33:13 +0200

    Update EntityListModel.java

--- src/main/java/ulb/lisa/infoh400/labs2022/view/EntityListModel.java
+++ src/main/java/ulb/lisa/infoh400/labs2022/view/EntityListModel.java
@@ -14,7 +14,10 @@ import javax.swing.AbstractListModel;
  * @author Adrien Foucart
  */
 public class EntityListModel<T> extends AbstractListModel {
-    
+ /*utilisation de templates = peut être remplacé par n'importe quelle classe*/
+ /*<T> peut être un patient, docteur...*/ 
+ /*va contenir une liste de n'importe quelle entité 
+ et utiliser les éléments de cette liste pour remplir le jlist. */ 
     private List<T> entities;
     
     public EntityListModel(List<T> entities){
@@ -31,11 +34,13 @@ public class EntityListModel<T> extends AbstractListModel {
     public List<T> getList(){
         return entities;
     }
-    
+/*2 methodes qui viennent de classe abstract list model*/    
+/*va dire à l'interface quelle est la taille de la liste*/    
     @Override
     public int getSize() {
         return entities.size();
     }
+/*va dire quel élément on trouve à un certain index*/ 
 
     @Override
     public Object getElementAt(int index) {
