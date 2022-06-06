10879d2684b0f97cf77ac19079af21a7e0ae2401
maximedg9 <91827637+maximedg9@users.noreply.github.com>
2022-06-06 10:42:36 +0200

    Update NonexistentEntityException.java

--- src/main/java/ulb/lisa/infoh400/labs2022/controller/exceptions/NonexistentEntityException.java
+++ src/main/java/ulb/lisa/infoh400/labs2022/controller/exceptions/NonexistentEntityException.java
@@ -1,4 +1,5 @@
 package ulb.lisa.infoh400.labs2022.controller.exceptions;
+/*quand une entité existe pas dans la database */
 
 public class NonexistentEntityException extends Exception {
     public NonexistentEntityException(String message, Throwable cause) {
