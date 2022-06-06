27f5ff104ba1eae3a8ce23642a5c6e9488a9eacc
maximedg9 <91827637+maximedg9@users.noreply.github.com>
2022-06-06 10:41:48 +0200

    Update PreexistingEntityException.java

--- src/main/java/ulb/lisa/infoh400/labs2022/controller/exceptions/PreexistingEntityException.java
+++ src/main/java/ulb/lisa/infoh400/labs2022/controller/exceptions/PreexistingEntityException.java
@@ -1,4 +1,5 @@
 package ulb.lisa.infoh400.labs2022.controller.exceptions;
+/*quand on veut ajouter une entité qui existe déjà */
 
 public class PreexistingEntityException extends Exception {
     public PreexistingEntityException(String message, Throwable cause) {
